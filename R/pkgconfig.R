a62dc0eda9d8000cd9d09c69b8a96c442980ab73
hclimente <hclimente@gmail.com>
2018-01-31 14:52:17 +0100

    followed BiocCheck suggestions

--- R/pkgconfig.R
+++ R/pkgconfig.R
@@ -1,29 +1,24 @@
-.build_path <-
-  function(path)
-  {
+.build_path <- function(path) {
     if (.Platform$OS.type == "windows") {
-      path <- normalizePath(path)
-      if (grepl(' ', path, fixed=TRUE))
+        path <- normalizePath(path)
+    if (grepl(' ', path, fixed=TRUE))
         path <- utils::shortPathName(path)
-      path <- gsub("\\\\", "/", path)
+    path <- gsub("\\\\", "/", path)
     }
     path
-  }
+}
 
-.pkgMk <-
-  function()
-  {
+.pkgMk <- function() {
     if (.Platform$OS.type != "windows")
-      stop(".pkgMk() expects windows operating system")
+        stop(".pkgMk() expects windows operating system")
+
     path <- system.file(package="Rgin", "etc", .Platform$r_arch,
                         "Rgin.mk", mustWork=TRUE)
     .build_path(path)
-  }
+}
 
-.pkgLd <-
-  function()
-  {
+.pkgLd <- function() {
     path <- system.file(package="Rgin", "usrlib", .Platform$r_arch,
                         mustWork=TRUE)
     .build_path(path)
-  }
+}
